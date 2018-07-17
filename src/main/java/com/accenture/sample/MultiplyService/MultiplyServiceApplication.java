e26db93565f92e0d6cdc6757838e0b2ea3056270
nhoomtham <nhoomtham@gmail.com>
2018-07-17 13:54:27 +0700

    init

---
+++ src/main/java/com/accenture/sample/MultiplyService/MultiplyServiceApplication.java
@@ -0,0 +1,12 @@
+package com.accenture.sample.MultiplyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplyServiceApplication.class, args);
+	}
+}
